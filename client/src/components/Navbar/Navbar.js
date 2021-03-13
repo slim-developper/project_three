dcc3d700f96ae7148b31ffc737145d6dfa50b3f7
chebbi <slimingprod@gmail.com>
2021-03-13 20:18:35 +0000

    transformation1

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
 
-import memories from '../../images/memories.png';
+import memories from '../../images/project-icon.jpg';
 import * as actionType from '../../constants/actionTypes';
 import useStyles from './styles';
 
@@ -38,7 +38,7 @@ const Navbar = () => {
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Projects</Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
       <Toolbar className={classes.toolbar}>

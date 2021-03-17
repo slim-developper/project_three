936e62be40dd23ee537d52e9e6a77e41efd71ca5
chebbi <slimingprod@gmail.com>
2021-03-17 21:40:46 +0000

    visit-as-guest

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -58,12 +58,12 @@ const Navbar = () => {
 
     setCompete(JSON.parse(localStorage.getItem('profile')));
   }, [location]);
-
+  if (user?.result?.isUser){
   return (
     <div>
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Projects</Typography>
+        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Projects-Forum</Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
 
@@ -79,7 +79,33 @@ const Navbar = () => {
       </Toolbar>
       
       </AppBar>
-      </div>)
+      </div>)}
+      else return(
+        <div>
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <div className={classes.brandContainer}>
+            <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Visit as guest</Typography>
+            <img className={classes.image} src={memories} alt="icon" height="60" />
+          </div>
+    
+          <Toolbar className={classes.toolbar}>
+            {user?.result ? (
+              <div className={classes.profile}>
+                <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
+                <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
+                <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+              </div>):
+              (<div className={classes.profile}><Button component={Link} to="/auth" variant="contained" color="primary">Sign In as client </Button>
+           <Button component={Link} to="/compete" variant="contained" color="primary">Sign In as compete</Button></div>)}
+          </Toolbar>
+          
+          </AppBar>
+          </div>
+
+
+
+
+      )
       
      
 };
